df15f35b9d7b025b1c5c9c7de53dda3bd1a51638
Amit Upadhyay <amitu2016@gmail.com>
2025-11-24 17:56:15 +0530

    first commit

---
+++ src/main/java/com/demo/instrumentation/OpenTelemetryConfig.java
@@ -0,0 +1,4 @@
+package com.demo.instrumentation;
+
+public class OpenTelemetryConfig {
+}
---
+++ src/test/java/com/demo/instrumentation/ManualInstrumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.instrumentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualInstrumentationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
